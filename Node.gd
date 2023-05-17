07780a38ad79a3640a8ca196455eef3989e241e8
Mdl74fsre1hz <faliaga@dim.uchile.cl>
2023-05-17 14:26:23 -0400

    final

--- Node.gd
+++ Node.gd
@@ -3,18 +3,21 @@ extends Node
 @onready var ray_marcher = get_parent().find_child("RayMarcher")
 @export var speed : float = 3.
 
+var camera_lock : bool = false : set = camera_lock_set
+
+
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	T.origin += 3.*Vector3.FORWARD + 1.*Vector3.UP
+	camera_lock = false
 
 var T : Transform3D = Transform3D()
 
 func get_input_dir() -> Vector2:
 	var ans : Vector2 = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"): ans.y += 1
-	if Input.is_action_pressed("ui_down"): ans.y -= 1
-	if Input.is_action_pressed("ui_left"): ans.x -= 1
-	if Input.is_action_pressed("ui_right"): ans.x += 1
+	if Input.is_action_pressed("up"): ans.y += 1
+	if Input.is_action_pressed("down"): ans.y -= 1
+	if Input.is_action_pressed("left"): ans.x -= 1
+	if Input.is_action_pressed("right"): ans.x += 1
 	return ans
 
 func _process(delta: float) -> void:
@@ -31,13 +34,14 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		# there is mouse motion
 		# calculate new angle
-		var relative_rotation : Vector2 = event.relative * 2 * PI / 1000.
-		var euler     : Vector3 = T.basis.get_euler(EULER_ORDER_YXZ)
-		euler.x += relative_rotation.y
-		euler.y += relative_rotation.x
-		T.basis = Basis.from_euler(euler,EULER_ORDER_YXZ)
-		ray_marcher.set_camera_angle(T,scale)
-	
+		if(camera_lock):
+			var relative_rotation : Vector2 = event.relative * 2 * PI / 1000.
+			var euler     : Vector3 = T.basis.get_euler(EULER_ORDER_YXZ)
+			euler.x += relative_rotation.y
+			euler.y += relative_rotation.x
+			T.basis = Basis.from_euler(euler,EULER_ORDER_YXZ)
+			ray_marcher.set_camera_angle(T,scale)
+		
 	if event is InputEventMouseButton:
 		var wheel_dir : float = 0.
 		if event.button_index==MOUSE_BUTTON_WHEEL_DOWN:
@@ -53,3 +57,25 @@ func _input(event):
 			get_tree().quit()
 		if event.keycode == KEY_R:
 			get_tree().change_scene_to_file("res://main.tscn")
+
+
+func camera_lock_set(val : bool):
+	camera_lock = val
+	var label = $"../UI/MarginContainer/Control/VBoxContainer/HBoxContainer/MouseLock2"
+	if val:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		label.text = " UNLOCKED "
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+		label.text = " LOCKED "
+
+
+func _on_sub_viewport_container_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index==MOUSE_BUTTON_LEFT:
+			if event.is_pressed():
+				camera_lock = !camera_lock
+
+
+func _on_speed_slider_value_changed(value: float) -> void:
+	speed = value
--- rendering_scene.gd
+++ rendering_scene.gd
@@ -10,5 +10,41 @@ func set_camera_angle(T : Transform3D, sc: float):
 	var Q = T
 	Q.basis.x.x *= sc
 	Q.basis.y.y *= sc
-#	Q.basis.z.z *= sc
 	shader_material.set_shader_parameter("camera_rotation",Q)
+
+func _on_sky_color_picker_color_changed(color: Color) -> void:
+	shader_material.set_shader_parameter("sky_color",color)
+
+
+func _on_floor_color_picker_color_changed(color: Color) -> void:
+	shader_material.set_shader_parameter("floor_color",color)
+
+
+func _on_objects_color_picker_color_changed(color: Color) -> void:
+	shader_material.set_shader_parameter("object_color",color)
+
+
+func _on_fogs_color_picker_color_changed(color: Color) -> void:
+	shader_material.set_shader_parameter("fog_color",color)
+
+
+func _on_pixels_x_value_changed(value: float) -> void:
+	var x : int = int(value)
+	shader_material.set_shader_parameter("x_pixels",x)
+
+
+func _on_pixels_y_value_changed(value: float) -> void:
+	var y : int = int(value)
+	shader_material.set_shader_parameter("y_pixels",y)
+
+
+func _on_fog_slider_value_changed(value: float) -> void:
+	shader_material.set_shader_parameter("foggyness",value)
+
+func _on_fog_decay_slider_value_changed(value: float) -> void:
+	shader_material.set_shader_parameter("fog_decay",value)
+
+
+func _on_reflection_slider_value_changed(value: float) -> void:
+	shader_material.set_shader_parameter("reflection_index",value)
+
